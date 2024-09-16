a7b6590e3ed9e7aa5b7c49cace11394b84c108fd
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-16 19:31:14 +0530

    Create History.js

---
+++ AuctionHouse/auction-management-frontend/src/profile/History.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const History = () => {
+  return (
+    <div className="p-6">
+      <h2 className="mb-6 text-3xl font-bold">History</h2>
+      <div className="p-6 bg-white rounded-lg shadow">
+        <p>No transaction history available.</p>
+      </div>
+    </div>
+  );
+};
+
+export default History;
