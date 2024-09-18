a3bc2c62fe0a80fa5b590ed9fa01db349d3abe04
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-18 23:22:50 +0530

    Update Login.js

--- AuctionHouse/auction-management-frontend/src/auth/Login.js
+++ AuctionHouse/auction-management-frontend/src/auth/Login.js
@@ -10,67 +10,57 @@ const Login = () => {
   };
 
   return (
-    <div className="flex ">
-      <div className="flex flex-col w-full h-screen rounded-none md:flex-row ">
-        <div className="flex flex-col justify-center w-full p-16 border-2 md:w-2/5">
-          <p className="mb-4 text-xl font-bold text-center">
-            Login to your account
-          </p>
+    <div className="flex items-start justify-center w-full h-screen mt-4">
+      <div className="flex flex-col w-full h-full p-16 md:w-2/5 md:h-auto">
+        <p className="mb-4 text-xl font-bold text-center">
+          Welcome back to Auctions!
+        </p>
 
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label className="block text-xs font-medium text-gray-700">
-                Email
-              </label>
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="block w-full px-3 py-2 mt-1 text-xs border border-gray-300 focus:border-gray-600"
-                placeholder="Enter your email"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-xs font-medium text-gray-700">
-                Password
-              </label>
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="block w-full px-3 py-2 mt-1 text-xs border border-gray-300 focus:border-gray-600"
-                placeholder="Enter your password"
-                required
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-full px-4 py-1 text-white bg-gray-600 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
-            >
-              Sign In
-            </button>
-          </form>
-
-          <div className="mt-4 text-center">
-            <p className="text-sm text-gray-600">
-              Don’t have an account?{" "}
-              <Link
-                to="/register"
-                className="font-semibold text-gray-600 transition-colors duration-200 hover:text-black"
-              >
-                Sign up
-              </Link>
-            </p>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label className="block text-xs font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="block w-full px-3 py-2 mt-1 text-xs border border-gray-300 focus:border-gray-600"
+              placeholder="Enter your email"
+              required
+            />
           </div>
-        </div>
+          <div className="mb-4">
+            <label className="block text-xs font-medium text-gray-700">
+              Password
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="block w-full px-3 py-2 mt-1 text-xs border border-gray-300 focus:border-gray-600"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full px-4 py-1 text-sm text-white bg-gray-600 hover:bg-gray-800 "
+          >
+            Login to your account
+          </button>
+        </form>
 
-        <div className="hidden w-full md:block md:w-2/3">
-          {/* <img
-            src={antiqueVase}
-            alt="Login visual"
-            className="object-cover w-full h-screen"
-          /> */}
+        <div className="mt-4 text-center">
+          <p className="text-xs text-gray-600 md:text-sm ">
+            Don’t have an account?{" "}
+            <Link
+              to="/register"
+              className="font-semibold text-gray-600 transition-colors duration-200 hover:text-black"
+            >
+              Sign up
+            </Link>
+          </p>
         </div>
       </div>
     </div>
