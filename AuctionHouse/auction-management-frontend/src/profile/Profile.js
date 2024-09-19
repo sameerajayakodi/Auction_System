a53c447e6076559e82928d7ecc8e34e387250ea8
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-19 22:57:14 +0530

    Update Profile.js

--- AuctionHouse/auction-management-frontend/src/profile/Profile.js
+++ AuctionHouse/auction-management-frontend/src/profile/Profile.js
@@ -11,7 +11,7 @@ const Profile = () => {
         <nav>
           <ul className="mt-40 space-y-10">
             <li className="mb-4">
-              <Link to="/profile/info" className="flex items-center text-xl">
+              <Link to="/profile/info" className="flex items-center text-lg">
                 <FaUser className="mr-2" /> {/* Replacing with FaUser icon */}
                 Profile
               </Link>
@@ -19,21 +19,21 @@ const Profile = () => {
             <li className="mb-4">
               <Link
                 to="/profile/password"
-                className="flex items-center text-xl"
+                className="flex items-center text-lg"
               >
                 <FaLock className="mr-2" /> {/* Replacing with FaLock icon */}
                 Password
               </Link>
             </li>
             <li className="mb-4">
-              <Link to="/profile/payment" className="flex items-center text-xl">
+              <Link to="/profile/payment" className="flex items-center text-lg">
                 <FaCreditCard className="mr-2" />{" "}
                 {/* Replacing with FaCreditCard icon */}
                 Payment
               </Link>
             </li>
             <li className="mb-4">
-              <Link to="/profile/history" className="flex items-center text-xl">
+              <Link to="/profile/history" className="flex items-center text-lg">
                 <FaHistory className="mr-2" />{" "}
                 {/* Replacing with FaHistory icon */}
                 History
