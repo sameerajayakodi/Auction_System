88be3ce4914e0bf51f98fa67a344545e6cda0b69
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-09-19 22:45:45 +0530

    Update History.js

--- AuctionHouse/auction-management-frontend/src/profile/History.js
+++ AuctionHouse/auction-management-frontend/src/profile/History.js
@@ -3,8 +3,8 @@ import React from "react";
 const History = () => {
   return (
     <div className="p-6">
-      <h2 className="mb-6 text-3xl font-bold">History</h2>
-      <div className="p-6 bg-white rounded-lg shadow">
+      <h2 className="mb-6 text-2xl font-bold">History</h2>
+      <div className="p-6 bg-white border-2">
         <p>No transaction history available.</p>
       </div>
     </div>
