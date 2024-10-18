1dfaec210b72a5f157c72f3c06a1194d3281d08e
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-18 09:38:20 +0530

    Update App.js

--- AuctionHouse/auction-management-frontend/src/App.js
+++ AuctionHouse/auction-management-frontend/src/App.js
@@ -15,50 +15,43 @@ import Login from "./auth/Login";
 import Register from "./auth/Register";
 import Navbar from "./components/Navbar";
 import Home from "./Home";
-import History from "./profile/History"; // Existing history component
-import Password from "./profile/Password"; // Existing password component
-import Payment from "./profile/Payment"; // Existing payment component
+import History from "./profile/History";
+import Password from "./profile/Password";
+import Payment from "./profile/Payment";
 import Profile from "./profile/Profile";
-import ProfileInfo from "./profile/ProfileInfo"; // Create a separate component for profile info
+import ProfileInfo from "./profile/ProfileInfo";
+import { UserProvider } from "./profile/UserContext"; // Import the context provider
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auctions" element={<AuctionListing />} />
-          <Route path="/auctions/:id" element={<AuctionDetail />} />
-
-          {/* Profile route with nested sub-routes */}
-
-          {/* Profile route with nested child routes */}
-          <Route path="/profile" element={<Profile />}>
-            {/* Redirect /profile to /profile/info */}
-            <Route index element={<Navigate to="/profile/info" />} />
-
-            {/* Define other child routes under /profile */}
-            <Route path="info" element={<ProfileInfo />} />
-            <Route path="password" element={<Password />} />
-            <Route path="payment" element={<Payment />} />
-            <Route path="history" element={<History />} />
-          </Route>
-
-          {/* You can define other routes here, if needed */}
-
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-
-          {/* Admin routes */}
-          <Route path="/admin/*" element={<AdminDashboard />}>
-            <Route path="manage-auctions" element={<ManageAuctions />} />
-            <Route path="manage-users" element={<ManageUsers />} />
-            <Route path="edit-user/:userId" element={<EditUser />} />
-          </Route>
-        </Routes>
-      </div>
-    </Router>
+    <UserProvider>
+      {" "}
+      {/* Wrap the app with UserProvider */}
+      <Router>
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auctions" element={<AuctionListing />} />
+            <Route path="/auctions/:id" element={<AuctionDetail />} />
+            <Route path="/profile" element={<Profile />}>
+              <Route index element={<Navigate to="/profile/info" />} />
+              <Route path="info" element={<ProfileInfo />} />
+              <Route path="password" element={<Password />} />
+              <Route path="payment" element={<Payment />} />
+              <Route path="history" element={<History />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/admin/*" element={<AdminDashboard />}>
+              <Route path="manage-auctions" element={<ManageAuctions />} />
+              <Route path="manage-users" element={<ManageUsers />} />
+              <Route path="edit-user/:userId" element={<EditUser />} />
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    </UserProvider>
   );
 }
 
