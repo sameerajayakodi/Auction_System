fdd004b68cfceba012123298d9008bbef8ef57df
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-18 14:36:32 +0530

    Update AddAuctionModal.js

--- AuctionHouse/auction-management-frontend/src/admin/AddAuctionModal.js
+++ AuctionHouse/auction-management-frontend/src/admin/AddAuctionModal.js
@@ -3,11 +3,14 @@ import Modal from "react-modal";
 
 Modal.setAppElement("#root");
 
-const AddAuctionModal = ({ isOpen, onClose, auction }) => {
+const AddAuctionModal = ({ isOpen, onClose, auction, saveAuctionData }) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [startingBid, setStartingBid] = useState("");
+  const [currentBid, setCurrentBid] = useState(0);
+  const [image, setImage] = useState("");
   const [endDate, setEndDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [status, setStatus] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const apiEndpoint = "https://localhost:44377/api/auction";
@@ -16,24 +19,49 @@ const AddAuctionModal = ({ isOpen, onClose, auction }) => {
     if (auction) {
       setTitle(auction.title);
       setDescription(auction.description);
-      setStartingBid(auction.startingBid);
+      setCurrentBid(auction.currentBid);
+      setImage(auction.image);
       setEndDate(auction.endDate);
+      setCategory(auction.category);
+      setStatus(auction.status);
     } else {
       setTitle("");
       setDescription("");
-      setStartingBid("");
+      setCurrentBid(0);
+      setImage("");
       setEndDate("");
+      setCategory("");
+      setStatus("");
     }
   }, [auction]);
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result); // Convert the file to a base64 URL
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   const handleSave = async () => {
-    if (!title || !description || !startingBid || !endDate) {
+    if (
+      !title ||
+      !description ||
+      !currentBid ||
+      !image ||
+      !endDate ||
+      !category ||
+      !status
+    ) {
       setError("All fields are required");
       return;
     }
 
-    if (isNaN(startingBid) || startingBid <= 0) {
-      setError("Starting bid must be a positive number");
+    if (isNaN(currentBid) || currentBid < 0) {
+      setError("Current bid must be a non-negative number");
       return;
     }
 
@@ -45,7 +73,15 @@ const AddAuctionModal = ({ isOpen, onClose, auction }) => {
     setLoading(true);
     setError(null);
 
-    const auctionData = { title, description, startingBid, endDate };
+    const auctionData = {
+      title,
+      description,
+      currentBid: parseFloat(currentBid),
+      image,
+      endDate,
+      category,
+      status,
+    };
 
     try {
       const response = await fetch(apiEndpoint, {
@@ -68,7 +104,9 @@ const AddAuctionModal = ({ isOpen, onClose, auction }) => {
 
       const result = await response.json();
       console.log("Auction saved successfully:", result);
-      onClose(); // Close modal after success
+
+      saveAuctionData(result);
+      onClose();
     } catch (error) {
       setError(error.message);
     } finally {
@@ -81,17 +119,17 @@ const AddAuctionModal = ({ isOpen, onClose, auction }) => {
       isOpen={isOpen}
       onRequestClose={onClose}
       contentLabel="Add/Edit Auction"
-      className="fixed inset-0 z-50 flex items-center justify-center"
+      className="fixed inset-0 z-50 flex items-center justify-center px-6"
       overlayClassName="fixed inset-0 bg-black bg-opacity-50"
     >
-      <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg">
+      <div className="w-full max-w-4xl p-6 bg-white rounded-lg shadow-lg">
         <h2 className="mb-4 text-2xl font-bold">
           {auction ? "Edit Auction" : "Add New Auction"}
         </h2>
         {error && <div className="mb-4 text-red-500">{error}</div>}
-        <form>
-          <div className="mb-4">
-            <label className="block mb-2 text-lg font-semibold" htmlFor="title">
+        <form className="grid grid-cols-2 gap-6">
+          <div className="col-span-1">
+            <label className="block mb-2 text-sm font-semibold" htmlFor="title">
               Title
             </label>
             <input
@@ -99,13 +137,29 @@ const AddAuctionModal = ({ isOpen, onClose, auction }) => {
               id="title"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
-              className="w-full p-3 border border-gray-300 rounded"
+              className="w-full p-2 border border-gray-300 rounded"
               required
             />
           </div>
-          <div className="mb-4">
+          <div className="col-span-1">
             <label
-              className="block mb-2 text-lg font-semibold"
+              className="block mb-2 text-sm font-semibold"
+              htmlFor="currentBid"
+            >
+              Current Bid
+            </label>
+            <input
+              type="number"
+              id="currentBid"
+              value={currentBid}
+              onChange={(e) => setCurrentBid(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
+              required
+            />
+          </div>
+          <div className="col-span-2">
+            <label
+              className="block mb-2 text-sm font-semibold"
               htmlFor="description"
             >
               Description
@@ -114,30 +168,27 @@ const AddAuctionModal = ({ isOpen, onClose, auction }) => {
               id="description"
               value={description}
               onChange={(e) => setDescription(e.target.value)}
-              className="w-full p-3 border border-gray-300 rounded"
-              rows="4"
-              required
-            ></textarea>
-          </div>
-          <div className="mb-4">
-            <label
-              className="block mb-2 text-lg font-semibold"
-              htmlFor="startingBid"
-            >
-              Starting Bid
-            </label>
-            <input
-              type="number"
-              id="startingBid"
-              value={startingBid}
-              onChange={(e) => setStartingBid(e.target.value)}
-              className="w-full p-3 border border-gray-300 rounded"
+              className="w-full p-2 border border-gray-300 rounded"
+              rows="3"
               required
             />
           </div>
-          <div className="mb-4">
+          <div className="col-span-1">
+            <label className="block mb-2 text-sm font-semibold" htmlFor="image">
+              Select Image
+            </label>
+            <input
+              type="file"
+              id="image"
+              accept="image/*"
+              onChange={handleFileChange}
+              className="w-full p-2 border border-gray-300 rounded"
+              required
+            />
+          </div>
+          <div className="col-span-1">
             <label
-              className="block mb-2 text-lg font-semibold"
+              className="block mb-2 text-sm font-semibold"
               htmlFor="endDate"
             >
               End Date
@@ -147,15 +198,47 @@ const AddAuctionModal = ({ isOpen, onClose, auction }) => {
               id="endDate"
               value={endDate}
               onChange={(e) => setEndDate(e.target.value)}
-              className="w-full p-3 border border-gray-300 rounded"
+              className="w-full p-2 border border-gray-300 rounded"
               required
             />
           </div>
-          <div className="flex justify-end space-x-4">
+          <div className="col-span-1">
+            <label
+              className="block mb-2 text-sm font-semibold"
+              htmlFor="category"
+            >
+              Category
+            </label>
+            <input
+              type="text"
+              id="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
+              required
+            />
+          </div>
+          <div className="col-span-1">
+            <label
+              className="block mb-2 text-sm font-semibold"
+              htmlFor="status"
+            >
+              Status
+            </label>
+            <input
+              type="text"
+              id="status"
+              value={status}
+              onChange={(e) => setStatus(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
+              required
+            />
+          </div>
+          <div className="flex justify-end col-span-2 mt-4 space-x-4">
             <button
               type="button"
               onClick={handleSave}
-              className="px-4 py-2 text-white bg-blue-500 rounded-lg"
+              className="px-6 py-2 text-white bg-blue-500 rounded-lg"
               disabled={loading}
             >
               {loading ? "Saving..." : "Save"}
@@ -163,7 +246,7 @@ const AddAuctionModal = ({ isOpen, onClose, auction }) => {
             <button
               type="button"
               onClick={onClose}
-              className="px-4 py-2 text-white bg-gray-500 rounded-lg"
+              className="px-6 py-2 text-white bg-gray-500 rounded-lg"
             >
               Cancel
             </button>
