a9977c358b11f72c20c611caa2c37153db5db0f7
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-10-24 14:52:52 +0530

    Update ProfileInfo.js

--- AuctionHouse/auction-management-frontend/src/profile/ProfileInfo.js
+++ AuctionHouse/auction-management-frontend/src/profile/ProfileInfo.js
@@ -1,69 +1,86 @@
 import React, { useContext, useState } from "react";
-import { UserContext } from "../auth/UserContext"; // Import UserContext
+import { UserContext } from "../auth/UserContext";
 import ConfirmationModal from "../components/ConfirmationModal";
-import profile from "../images/1 (5).jpg"; // Profile photo path
-import cover from "../images/registerImage.jpg"; // Cover photo path
+import profile from "../images/1 (5).jpg";
+import cover from "../images/registerImage.jpg";
 
 const ProfileInfo = () => {
-  const { user } = useContext(UserContext); // Access user from UserContext
+  const { user, setUser } = useContext(UserContext);
   const [isEditing, setIsEditing] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [profileInfo, setProfileInfo] = useState({
-    fullName: user?.fullName || "", // Use user data from context
+    fullName: user?.fullName || "",
     email: user?.email || "",
     phone: user?.phone || "",
     location: user?.location || "",
-    address: user?.address || "", // Changed from bio to address
+    address: user?.address || "",
   });
 
-  const handleEditClick = () => {
-    setIsEditing(true);
-  };
-
-  const handleCancelClick = () => {
-    setIsEditing(false);
-  };
-
-  const handleSaveClick = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleModalConfirm = async () => {
-    try {
-      const userId = user?.id; // Get user ID from the context
-      const apiUrl = `https://localhost:44377/api/users/UpdateUser/${userId}`; // API endpoint with user ID
-
-      const response = await fetch(apiUrl, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(profileInfo),
-      });
-
-      if (response.ok) {
-        setIsEditing(false);
-      } else {
-        console.error("Failed to save profile data");
-      }
-    } catch (error) {
-      console.error("Failed to save profile data:", error);
-    }
-
-    setIsModalOpen(false);
-  };
-
-  const handleModalClose = () => {
-    setIsModalOpen(false);
-  };
+  const handleEditClick = () => setIsEditing(true);
+  const handleCancelClick = () => setIsEditing(false);
 
   const handleChange = (e) => {
     setProfileInfo({ ...profileInfo, [e.target.name]: e.target.value });
   };
 
+  const handleModalClose = () => setIsModalOpen(false);
+
+  const createPayload = () => ({
+    id: user?.id,
+    fullName: profileInfo.fullName || null,
+    email: profileInfo.email,
+    password: user?.password || "string",
+    address: profileInfo.address || null,
+    location: profileInfo.location || null,
+    phone: profileInfo.phone || "string",
+  });
+
+  const handleSaveClick = () => {
+    // Basic validation check (you can enhance this further)
+    if (!profileInfo.email) {
+      setError("Email is required.");
+      return;
+    }
+
+    setIsModalOpen(true);
+  };
+
+  const handleModalConfirm = async () => {
+    setLoading(true);
+    setError(null);
+    const apiUrl = `https://localhost:44377/api/users/UpdateUser/${user?.id}`;
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(createPayload()),
+      });
+
+      if (response.ok) {
+        const updatedUser = { ...user, ...profileInfo };
+        setUser(updatedUser);
+        setIsEditing(false);
+      } else {
+        const errorData = await response.json();
+        setError(errorData.message || "Failed to save profile data.");
+      }
+    } catch (error) {
+      setError("Failed to save profile data. Please try again later.");
+    } finally {
+      setLoading(false);
+      setIsModalOpen(false);
+    }
+  };
+
   return (
     <div className="flex flex-col">
       <h2 className="mb-2 text-2xl font-bold">Profile Information</h2>
+      {error && <p className="text-red-500">{error}</p>}
       <div className="p-6 bg-white border-2">
         <div className="relative mb-2">
           <img
@@ -80,57 +97,28 @@ const ProfileInfo = () => {
           </div>
         </div>
         <div className="grid grid-cols-2 gap-6">
-          <div className="col-span-1">
-            <label className="text-gray-700">Full Name</label>
-            <input
-              type="text"
-              name="fullName"
-              value={profileInfo.fullName}
-              onChange={handleChange}
-              className="w-full p-2 mt-1 border-2 rounded-md"
-              readOnly={!isEditing}
-            />
-          </div>
-          <div className="col-span-1">
-            <label className="text-gray-700">Email</label>
-            <input
-              type="email"
-              name="email"
-              value={profileInfo.email}
-              onChange={handleChange}
-              className="w-full p-2 mt-1 border-2"
-              readOnly={!isEditing}
-            />
-          </div>
-          <div className="col-span-1">
-            <label className="text-gray-700">Phone</label>
-            <input
-              type="text"
-              name="phone"
-              value={profileInfo.phone}
-              onChange={handleChange}
-              className="w-full p-2 mt-1 border-2"
-              readOnly={!isEditing}
-            />
-          </div>
-          <div className="col-span-1">
-            <label className="text-gray-700">Location</label>
-            <input
-              type="text"
-              name="location"
-              value={profileInfo.location}
-              onChange={handleChange}
-              className="w-full p-2 mt-1 border-2"
-              readOnly={!isEditing}
-            />
-          </div>
+          {["fullName", "email", "phone", "location"].map((field, index) => (
+            <div className="col-span-1" key={index}>
+              <label className="text-gray-700">
+                {field.charAt(0).toUpperCase() + field.slice(1)}
+              </label>
+              <input
+                type={field === "email" ? "email" : "text"}
+                name={field}
+                value={profileInfo[field]}
+                onChange={handleChange}
+                className="w-full p-2 mt-1 border-2 rounded-md"
+                readOnly={!isEditing}
+              />
+            </div>
+          ))}
           <div className="col-span-2">
             <label className="text-gray-700">Address</label>
             <textarea
               name="address"
               value={profileInfo.address}
               onChange={handleChange}
-              className="w-full p-2 mt-1 border-2"
+              className="w-full p-2 mt-1 border-2 rounded-md"
               readOnly={!isEditing}
             />
           </div>
@@ -148,8 +136,9 @@ const ProfileInfo = () => {
               <button
                 className="px-4 py-2 mr-2 text-white bg-green-500 rounded-md hover:bg-green-600"
                 onClick={handleSaveClick}
+                disabled={loading}
               >
-                Save
+                {loading ? "Saving..." : "Save"}
               </button>
               <button
                 className="px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600"
